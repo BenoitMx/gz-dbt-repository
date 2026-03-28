ce70b62052171b62e3adad6ac6367ad436fbaf75
benoitbernardgerard@gmail.com <benoitbernardgerard@gmail.com>
2026-03-28 18:17:43 +0000

    where shippingfee is differente than shipping_fee1

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_sales.sql
@@ -0,0 +1,24 @@
+with 
+
+ -- stg_raw__sales.sql
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'raw_gz_sales') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        orders_id,
+        pdt_id AS product_id,
+        revenue,
+        quantity
+
+    from source
+
+)
+
+select * from renamed
