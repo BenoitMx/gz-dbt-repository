0af6206b9f6c10cb3106a28c998ada4d739ed8e1
benoitbernardgerard@gmail.com <benoitbernardgerard@gmail.com>
2026-03-30 23:33:17 +0000

    the following data at a daily granularity: Date Total number of transactions Total revenue Average Basket Operational Margin Total purchase cost Total shipping fees Total log costs Total quantity of products sold.

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,15 +1,20 @@
---Operational_margin = margin + shipping_fee - log_cost - ship_cost
+-- Operational_margin = margin + shipping_fee - log_cost - ship_cost
 
 SELECT
-a.orders_id
-,a.date_date
-,ROUND(
-    a.margin 
-    + SAFE_CAST(b.shipping_fee as FLOAT64) 
-    - SAFE_CAST(b.logcost as FLOAT64) 
-    - SAFE_CAST(b.ship_cost as FLOAT64)
-    , 2) AS operational_margin
-FROM {{ref('int_orders_margin')}} as a
-JOIN {{ref('stg_gz_raw_data__raw_gz_ship')}} as b
+  a.orders_id,
+  a.date_date,
+  a.revenue,
+  a.quantity,
+  a.purchase_cost AS total_purchase_cost,
+  SAFE_CAST(b.shipping_fee AS FLOAT64) AS total_ship_fee,
+  SAFE_CAST(b.logcost AS FLOAT64) AS total_log_costs,
+  ROUND(
+    a.margin
+    + SAFE_CAST(b.shipping_fee AS FLOAT64)
+    - SAFE_CAST(b.logcost AS FLOAT64)
+    - SAFE_CAST(b.ship_cost AS FLOAT64),
+    2
+  ) AS operational_margin
+FROM {{ ref('int_orders_margin') }} AS a
+JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }} AS b
 USING (orders_id)
-ORDER BY orders_id DESC
